0ee788187b2bd361bff40cb63bbcfb6fe3849235
VennelaReddy408 <podduturivennela@gmail.com>
2024-10-24 10:58:59 +0530

    Add files via upload

---
+++ control.js
@@ -0,0 +1,52 @@
+// // examples for for loop
+for(var a=1;a<=5;a++){
+    console.log(a)
+}
+
+for(var a=5;a>=2;a--){
+    console.log(a)
+}
+
+for(var a=0;a<=5;a++){
+}
+console.log(a)
+
+// examples of while loop
+var a=1
+while(a<=3){
+    console.log(a)
+    a++
+}
+var a=10
+while(a>=6){
+    console.log(a)
+    a--
+}
+
+var a=1
+while(a<4){
+    a++
+}
+console.log(a)
+
+// examples of do while  
+var a=1
+do{
+    a++
+    console.log(a)
+}while(a<=5)
+
+
+    var i=10
+    do{
+        i--
+        console.log(i)
+    }while(i>=7)
+        
+
+    var a=10
+    do{
+        
+        console.log(a)
+        a--
+    }while(a>7)
